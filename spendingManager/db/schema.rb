dbc3116feb3f0497a9d0c3bf161a9a22d4aa4327
HEUICHAN(TERRNCE) LIM <hlim1@mail.csuchico.edu>
2017-09-07 00:53:31 -0700

    Updated applicationController.rb to permit additional user info when registration.

--- spendingManager/db/schema.rb
+++ spendingManager/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170901064239) do
+ActiveRecord::Schema.define(version: 20170906090007) do
 
   create_table "mains", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -30,6 +30,8 @@ ActiveRecord::Schema.define(version: 20170901064239) do
     t.string "last_sign_in_ip"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "firstname"
+    t.string "lastname"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
