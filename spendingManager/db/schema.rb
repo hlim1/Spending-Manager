4fa3e37d73ea67b9a9a265ff588c38b328930a2f
HEUICHAN(TERRNCE) LIM <hlim1@mail.csuchico.edu>
2017-09-10 04:01:57 -0700

    Created Summary page

--- spendingManager/db/schema.rb
+++ spendingManager/db/schema.rb
@@ -10,11 +10,28 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170906090007) do
+ActiveRecord::Schema.define(version: 20170910110046) do
+
+  create_table "accounts", force: :cascade do |t|
+    t.string "name"
+    t.integer "accountNumber"
+    t.decimal "balance"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_accounts_on_user_id"
+  end
 
   create_table "mains", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "summaries", force: :cascade do |t|
+    t.integer "bank_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["bank_id"], name: "index_summaries_on_bank_id"
   end
 
   create_table "users", force: :cascade do |t|
