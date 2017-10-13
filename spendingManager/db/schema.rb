0ec921c90ef850fe3f553cf425569140da68cc75
HEUICHAN(TERRNCE) LIM <hlim1@mail.csuchico.edu>
2017-10-13 12:51:49 -0700

    Created a trigger to update daily_totals

--- spendingManager/db/schema.rb
+++ spendingManager/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170925015745) do
+ActiveRecord::Schema.define(version: 20171013011232) do
 
   create_table "accounts", force: :cascade do |t|
     t.string "name"
