fd44d4893d227b413aa690f6c41bfccbd2755f10
HEUICHAN(TERRNCE) LIM <hlim1@mail.csuchico.edu>
2017-11-13 00:39:51 -0800

    Switching charting method from pure google chart to Chartkick.

--- spendingManager/app/controllers/statistics_controller.rb
+++ spendingManager/app/controllers/statistics_controller.rb
@@ -19,63 +19,64 @@ class StatisticsController < ApplicationController
       } # end of inner dailyTotal loop
     } # end of monthlyTotal loop
 
-    mTotals = Array.new
+    @mTotals = Array.new
     monthlyTotals.each { |mt|
       case mt.month
       when 1
-        mTotals.push(["January",mt.total.to_f])
+        @mTotals.push(["January",mt.total.to_f])
       when 2
-        mTotals.push(["February",mt.total.to_f])
+        @mTotals.push(["February",mt.total.to_f])
       when 3
-        mTotals.push(["March",mt.total.to_f])
+        @mTotals.push(["March",mt.total.to_f])
       when 4
-        mTotals.push(["April",mt.total.to_f])
+        @mTotals.push(["April",mt.total.to_f])
       when 5
-        mTotals.push(["May",mt.total.to_f])
+        @mTotals.push(["May",mt.total.to_f])
       when 6
-        mTotals.push(["June",mt.total.to_f])
+        @mTotals.push(["June",mt.total.to_f])
       when 7
-        mTotals.push(["July",mt.total.to_f])
+        @mTotals.push(["July",mt.total.to_f])
       when 8
-        mTotals.push(["August",mt.total.to_f])
+        @mTotals.push(["August",mt.total.to_f])
       when 9
-        mTotals.push(["September",mt.total.to_f])
+        @mTotals.push(["September",mt.total.to_f])
       when 10
-        mTotals.push(["October",mt.total.to_f])
+        @mTotals.push(["October",mt.total.to_f])
       when 11
-        mTotals.push(["November",mt.total.to_f])
+        @mTotals.push(["November",mt.total.to_f])
       when 12
-        mTotals.push(["December",mt.total.to_f])
+        @mTotals.push(["December",mt.total.to_f])
       end
     }
-    gon.mTotals = mTotals
+    # gon.mTotals = mTotals
 
     # Extracting only expenses types in order to remove "None" type, which considers to be a Credit
-    spendingTypes = {"Food" => 0, "Education" => 0, "Transportation" => 0, "Rent/Maintenance" => 0, "Travle" => 0, "Luxuries" => 0, "Other" => 0}
+    @spendingTypes = {"Food" => 0, "Education" => 0, "Transportation" => 0, "Rent/Maintenance" => 0, "Travle" => 0, "Luxuries" => 0, "Other" => 0}
     breakdowns = Breakdown.all
     breakdowns.each { |breakdown|
       if (breakdown.spendingType == "Food")
-        spendingTypes["Food"] += 1
+        @spendingTypes["Food"] += 1
       elsif (breakdown.spendingType == "Education")
-        spendingTypes["Education"] += 1
+        @spendingTypes["Education"] += 1
       elsif (breakdown.spendingType == "Transportation")
-        spendingTypes["Transportation"] += 1
+        @spendingTypes["Transportation"] += 1
       elsif (breakdown.spendingType == "Rent/maintenance")
-        spendingTypes["Rent/maintenance"] += 1
+        @spendingTypes["Rent/maintenance"] += 1
       elsif (breakdown.spendingType == "Travle")
-        spendingTypes["Travle"] += 1
+        @spendingTypes["Travle"] += 1
       elsif (breakdown.spendingType == "Luxuries")
-        spendingTypes["Luxuries"] += 1
+        @spendingTypes["Luxuries"] += 1
       elsif (breakdown.spendingType == "Travle")
-        spendingTypes["Other"] += 1
+        @spendingTypes["Other"] += 1
       end
     }
-
+=begin
     dataForCharts = Array.new
     spendingTypes.each { |key, value|
       dataForCharts.push([key, value])
     }
     gon.spendingTypes = dataForCharts;
+=end
   end
 
   # GET /statistics/1
